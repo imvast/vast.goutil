e9111d6656ee08085147efae5baaf44d9953950f
VΛSƬ <60797067+imvast@users.noreply.github.com>
2023-05-22 20:33:13 -0400

    Update logger.go

--- logger.go
+++ logger.go
@@ -19,7 +19,7 @@ import (
 )
 
 
-func GetTime(c bool) string {
+func getTime(c bool) string {
 	currentTime := time.Now().Format("15:04:05")
 	if c == true {
 		return color.HiBlackString(currentTime)
